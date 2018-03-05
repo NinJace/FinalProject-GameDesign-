58160a6533e479ca029550105790f38f32721d70
SethPain <sethpain2@gmail.com>
2018-03-05 16:35:04 -0600

    Working UI Elements

---
+++ ObjectOrientedDating/Assets/Ryn/Scripts/Moves.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moves : MonoBehaviour {
+
+    GameObject manager;
+
+    PuzzleManagerScript script;
+    Text movesOrWhatever;
+
+    private void Awake()
+    {
+        manager = GameObject.Find("Puzzle Manager");
+        script = manager.GetComponent<PuzzleManagerScript>();
+        this.GetComponent<Text>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        movesOrWhatever.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
