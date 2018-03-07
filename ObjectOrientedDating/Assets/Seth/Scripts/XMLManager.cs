798e33fe7340da7619765806472919290eab5ccb
SethPain <sethpain2@gmail.com>
2018-03-07 16:34:34 -0600

    Also Testing XML

---
+++ ObjectOrientedDating/Assets/Seth/Scripts/XMLManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class XMLManager : MonoBehaviour {
+
+    public static XMLManager ins;
+
+    void Awake()
+    {
+        ins = this;
+    }
+
+    public ConvoDatabase convoDB;
+
+    
+}
+
+[System.Serializable]
+public class ConvoEntry
+{
+    public string optionA;
+
+    public string optionB;
+
+    public string optionC;
+}
+
+[System.Serializable]
+public class ConvoDatabase
+{
+    public List<ConvoEntry> list = new List<ConvoEntry>();
+}
